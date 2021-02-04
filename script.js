7b881eaba057523a2c9b3ccfb77ddf6ebc275df2
rkuchenbecker <rkuchenbecker@bbw-fi.de>
2021-02-04 10:06:48 +0100

    edited setUsername function added function "getUsername" added TimerInterval for AJAX

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function setUsername() {
     document.querySelector('input[name=name]').value = localStorage.username;
 }
 onload = setUsername;
+
+function getUsername() {
+    var name = localStorage.username
+    $.post( "eingabe.php" , {name: name} );
+}
+
+setInterval(function() {
+    $.ajax("index.html")
+}, 5000)
